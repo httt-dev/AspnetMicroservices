a100c824dc6762ac630e8ed55f8de878f9c079cc
hoanguyen <httt.dev@gmail.com>
2022-01-16 21:53:39 +0700

    API Gateway Shopping Aggregation

---
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketItemExtenndedModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Models
+{
+    public class BasketItemExtenndedModel
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+
+        //Product related additional fields
+        public string Category { get; set; }
+        public string Summary { get; set; }
+
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Models
+{
+    public class BasketModel
+    {
+        public string UserName { get; set; }
+        public List<BasketItemExtenndedModel> Items { get; set; } = new List<BasketItemExtenndedModel>();
+        public decimal TotalPrice { get; set; }
+
+    }
+}
